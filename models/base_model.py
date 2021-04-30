95ff85e7a9202ee58aa1529eed92033d49cdcc20
Jenni-Foued <medfouedjenni@gmail.com>
2021-04-30 19:02:26 +0100

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -20,8 +20,8 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
-            self.created_at = self.updated_at =
-            datetime.strptime(kwargs["created_at"], date)
+            self.created_at = datetime.strptime(kwargs["created_at"], date)
+            self.updated_at = datetime.strptime(kwargs["updated_at"], date)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
